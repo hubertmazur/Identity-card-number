5adcdac9a56171f79598c9845335f18135f1036c
hubert <hubert.mazur@vp.pl>
2019-06-25 22:30:57 +0200

    Added PESEL validator.

---
+++ src/main/java/PESELValidator.java
@@ -0,0 +1,22 @@
+public class PESELValidator {
+
+    private static final int[] WEIGHTS = {9, 7, 3, 1, 9, 7, 3, 1, 9, 7};
+
+    public Boolean validate (String PESEL) {
+      PESEL = PESEL.trim();
+      if (!PESEL.matches("[0-1]{11}")) {
+          return false;
+      }
+      int temp = 0;
+        for (int i = 0; i < WEIGHTS.length; i++) {
+            int weight = WEIGHTS[i];
+            int digit = Character.digit(PESEL.charAt(i), 10);
+            temp += weight * digit;
+        }
+       int lastDigit = Character.digit(PESEL.charAt(PESEL.length()-1),10);
+
+        return  temp % 10 == lastDigit;
+    }
+
+
+}
---
+++ src/test/java/PESELValidatorTest.java
@@ -0,0 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class PESELValidatorTest {
+
+    private PESELValidator peselValidator;
+
+    @Before
+    public void setup () {
+        peselValidator = new PESELValidator();
+    }
+    @Test
+    public void test_lower_length() {
+
+    }
+    @Test
+    public void test_higher_length() {
+
+    }
+    @Test
+    public void test_PESEL_not_consisting_of_digits() {
+
+    }
+    @Test
+    public void test_correct_PESEL() {
+
+    }
+    @Test
+    public void test_incorrect_PESEL() {
+
+    }
+
+
+
+
+
+}
